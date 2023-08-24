23679d9dc5cfdf966a15e6fadb0cc664583e82af
Kanhaiya Kumar <kkanhaiya942@gmail.com>
2023-08-24 21:53:36 +0530

    Change code by kalvi

--- src/Students.java
+++ src/Students.java
@@ -1,32 +1,52 @@
 
 public class Students {
+	 
+	int binarrySearch(int []arr,int k,int start,int end 	)
+	{
+		
+		while(start<=end) {
+			int mid = start+ (end-start)/2;
+			 
+			
+		
+			if(arr[mid]==k)
+			
+				return mid;
+				
+			
+			else if(arr[mid]>k)
+		
+				end=mid-1;
+			
+			else 
+				start=mid+1;}
+			//int mid = start+ (end-start)/2;
+		
+		return -1;
+		
+	}
+	
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int [] arr= {1,3,7,9,10};
-		int k=9;
-		int start=0;
+		
+		//int start=0;
 		int end=arr.length;
-		
-		for(int i=0;i<end;i++) {
-		//while(start<=end){
-			int mid= start+(end-start)/2;
-			if(arr[mid]==k)
-			{
-				System.out.println(mid);
-			}
-			else if(k>arr[mid])
-			{
-				start=mid+1;
-			}
-			else {
-				end=mid-1;}
-			
-		}
+		int k=10;
+	    Students obj =new Students();
+	    int result=obj.binarrySearch(arr, k, 0, end-1);
+	    
+	    if (result==-1)
+	    System.out.println("does not found");
+	    else
+	    System.out.println("index number "+result);
+	    
 		
 		
-		System.out.println("data not found !");
-
-	}
-
+		
+		
+		
 }
+	}
